6e6719cb361a7f15639b27647ec2e31a84800b77
priyanka ajjampudi <ajjampudipriyanka19@gmail.com>
2020-07-24 16:21:41 +0530

    commit

---
+++ logging/src/org/apache/log4j/LogManager.java
@@ -0,0 +1,12 @@
+package org.apache.log4j;
+
+import logging.HouseConstruction;
+
+public class LogManager {
+
+	public static Logger getLogger(Class<HouseConstruction> class1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ logging/src/org/apache/log4j/Logger.java
@@ -0,0 +1,10 @@
+package org.apache.log4j;
+
+public class Logger {
+
+	public void debug(double si) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
