17ac384e08fa33f86762ad1ab0cf23eec49a4d23
priyanka ajjampudi <ajjampudipriyanka19@gmail.com>
2020-07-24 05:49:43 +0530

    commit

---
+++ 1234/src/main/java/choco/Sweets.java
@@ -0,0 +1,21 @@
+package choco;
+abstract class Sweets {
+    Chocolate chocolate;
+    OtherSweets otherSweets;
+    abstract int getWeight();
+    public Chocolate getChocolate() {
+        return chocolate;
+    }
+
+    public void setChocolate(Chocolate chocolate) {
+        this.chocolate = chocolate;
+    }
+
+    public OtherSweets getOtherSweets() {
+        return otherSweets;
+    }
+
+    public void setOtherSweets(OtherSweets otherSweets) {
+        this.otherSweets = otherSweets;
+    }
+} 
